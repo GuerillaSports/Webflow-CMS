13ca11378a0c183e46d6e56b3949e5d5fdad4586
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-09-23 13:35:11 -0600

    FIX: title contrast fixer
    Now fixes contrast on load and on lazy-load events

--- cdn/index.js
+++ cdn/index.js
@@ -1,30 +1,40 @@
-function manageColorContrasts() {
-  const cards = document.querySelectorAll(".card-label");
-  const gs_blue = getComputedStyle(cards[0]).getPropertyValue('--blue')
-  for (let card of cards) {
-    const on_color = card.querySelector(".title-on-color")
-    if (!on_color) continue;
-    if (on_color.style.color === "white") {
-      const story_title = card.querySelector(".title-blue")
-      story_title.style.color = gs_blue;
-    }
+function manageTitleColorContrast(card) {
+  const gs_blue = getComputedStyle(card).getPropertyValue('--blue')
+  const on_color = card.querySelector(".title-on-color")
+  if (!on_color) return;
+  if (on_color.style.color === "white") {
+    const story_title = card.querySelector(".title-blue")
+    story_title.style.color = gs_blue;
   }
-};
+}
 
 /**
- * Webflow cms list content is lazy loaded.
+ * Manage color contrast on initial load.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".card-label");
+  for (let card of cards) {
+    manageTitleColorContrast(card)
+  }
+})
+
+/**
+ * Webflow cms list content can be lazy loaded.
  * This requires us to use a mutation observer to watch for lazy-loaded content.
  * When the content changes, the contrast function is re-run.
  */
-const dynamic_lists = document.querySelector(".dynam-list")
+const dynamic_list = document.querySelector(".dynam-list")
 const config = { childList: true, subtree: true }
 const story_title_observer = new MutationObserver((mutList) => {
   for (let mut of mutList) {
     // check if element has been added
     if (mut.addedNodes.length) {
-      manageColorContrasts()
+      for (let card of mut.addedNodes) {
+        manageTitleColorContrast(card)
+
+      }
     }
   }
 })
 
-story_title_observer.observe(dynamic_lists, config)
+story_title_observer.observe(dynamic_list, config)
