008c3077e234bda2a9375838068659bb5463afaa
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-09-23 12:49:49 -0600

    MV_PHILOSOHPY: global, sitewide, file

---
+++ cdn/index.js
@@ -0,0 +1,25 @@
+function manageColorContrasts() {
+  const cards = document.querySelectorAll(".card-label");
+  const gs_blue = getComputedStyle(cards[0]).getPropertyValue('--blue')
+  for (let card of cards) {
+    const on_color = card.querySelector(".title-on-color")
+    if (!on_color) continue;
+    if (on_color.style.color === "white") {
+      const story_title = card.querySelector(".title-blue")
+      story_title.style.color = gs_blue;
+    }
+  }
+};
+
+
+const globalFunctions = {
+  manageColorContrasts
+}
+
+window.globalFunctions = globalFunctions
+
+document.addEventListener("DOMContentLoaded", function () {
+  for (let key of window.globalFunctions) {
+    window.globalFunctions[key]()
+  }
+})
